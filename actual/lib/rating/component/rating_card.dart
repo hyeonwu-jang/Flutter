8799bc7d27a4ed27526874ed7a4e392492c624fa
Hyeonu <jhw3603@naver.com>
2023-06-08 23:59:18 +0900

    와.. 개어렵네

--- actual/lib/rating/component/rating_card.dart
+++ actual/lib/rating/component/rating_card.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/const/colors.dart';
+import 'package:actual/rating/model/rating_model.dart';
 import 'package:flutter/material.dart';
 import 'package:collection/collection.dart';
 
@@ -25,6 +26,20 @@ class RatingCard extends StatelessWidget {
       Key? key})
       : super(key: key);
 
+  factory RatingCard.fromModel({
+    required RatingModel model,
+  }) {
+    return RatingCard(
+      avatarImage: NetworkImage(
+        model.user.imageUrl,
+      ),
+      images: model.imgUrls.map((e) => Image.network(e)).toList(),
+      rating: model.rating,
+      email: model.user.username,
+      content: model.content,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -40,13 +55,13 @@ class RatingCard extends StatelessWidget {
         _Body(
           content: content,
         ),
-        if(images.isNotEmpty)
-        SizedBox(
-          height: 100,
-          child: _Images(
-            images: images,
+        if (images.isNotEmpty)
+          SizedBox(
+            height: 100,
+            child: _Images(
+              images: images,
+            ),
           ),
-        ),
       ],
     );
   }
--- actual/lib/restaurant/view/restaurant_detail_screen.dart
+++ actual/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,12 +1,15 @@
 import 'package:actual/common/const/data.dart';
 import 'package:actual/common/dio/dio.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/model/cursor_pagination_model.dart';
 import 'package:actual/product/component/product_card.dart';
 import 'package:actual/rating/component/rating_card.dart';
+import 'package:actual/rating/model/rating_model.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_detail_model.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/provider/restaurant_provider.dart';
+import 'package:actual/restaurant/provider/restaurant_rating_provider.dart';
 import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -35,6 +38,7 @@ class _RestaurantDetailScreenState
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(restaurantDetailProvider(widget.id));
+    final ratingsState = ref.watch(restaurantRatingProvider(widget.id));
 
     if (state == null) {
       return DefaultLayout(
@@ -57,22 +61,31 @@ class _RestaurantDetailScreenState
             renderProduct(
               products: state.products,
             ),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            sliver: SliverToBoxAdapter(
-              child: RatingCard(
-                rating: 4,
-                email: 'jc@codefactory.ai',
-                images: [],
-                avatarImage: AssetImage(
-                  'asset/img/logo/codefactory_log.png'
-                ),
-                content: '맛있습니다.',
-              ),
+          if (ratingsState is CursorPagination<RatingModel>)
+            renderRatings(
+              models: ratingsState.data,
             ),
-          ),
         ],
       ),
+    );
+  }
+
+  SliverPadding renderRatings({
+    required List<RatingModel> models,
+  }) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (_, index) => Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: RatingCard.fromModel(
+              model: models[index],
+            ),
+          ),
+          childCount: models.length,
+        ),
+      )
     );
   }
 
