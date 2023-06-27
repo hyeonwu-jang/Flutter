e00e2f4cadbdeed78e4e6bfa2bbc9ad19e7a5217
Hyeonu <jhw3603@naver.com>
2023-06-28 00:42:31 +0900

    조금씩

---
+++ my_favorite_restaurant/android/app/src/main/kotlin/com/myfavorite_rstn/my_favorite_restaurant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myfavorite_rstn.my_favorite_restaurant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ my_favorite_restaurant/lib/common/layout/default_layout.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DefaultLayout extends StatelessWidget {
+  final String? title;
+  final Widget child;
+
+  const DefaultLayout({
+    this.title,
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: child,
+    );
+  }
+}
