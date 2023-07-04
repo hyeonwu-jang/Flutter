7d7f74305dcefad64d6cd1186f68276bf90f8950
Hyeonu <jhw3603@naver.com>
2023-07-05 01:14:04 +0900

    복습이나하자~~

--- my_favorite_restaurant/lib/common/component/custom_text_form_field.dart
+++ my_favorite_restaurant/lib/common/component/custom_text_form_field.dart
@@ -24,18 +24,21 @@ class CustomTextFormField extends StatelessWidget {
             fontSize: 14.0
         ),
         border: OutlineInputBorder(   // 폼을 선택했을 때 생기는 라인
+          borderRadius: BorderRadius.circular(18.0),
           borderSide: BorderSide(
             color: Colors.black,
             width: 55.0,
           )
         ),
-        enabledBorder: OutlineInputBorder(  // 기본적으로 보여지는 라인
-          borderSide: BorderSide(
-            color: Colors.black,
-            width: 1.0
-          )
-        ),
+        // enabledBorder: OutlineInputBorder(  // 기본적으로 보여지는 라인
+        //   borderRadius: BorderRadius.circular(32.0),
+        //   borderSide: BorderSide(
+        //     color: Colors.black,
+        //     width: 1.0
+        //   )
+        // ),
         errorText: errorText,
+
       ),
       onChanged: onChanged,
     );
--- my_favorite_restaurant/lib/user/view/login_screen.dart
+++ my_favorite_restaurant/lib/user/view/login_screen.dart
@@ -9,91 +9,94 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       backgroundColor: Colors.orange[300],
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 2,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '나만의 맛집',
-                  style: TextStyle(
-                    fontSize: 48.0,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
+      child: SingleChildScrollView(   // 키보드가 침범하는 영역 조정
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '나만의 맛집',
+                    style: TextStyle(
+                      fontSize: 48.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                Text(
-                  '내가 경험한 찐맛집을 저장하고 공유해요',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
+                  Text(
+                    '기억하고 싶은 맛집의 메뉴를 기록하고 공유해요',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _Circle(context),
-                    _Circle(context),
-                    _Circle(context),
-                    _Circle(context),
-                    _Circle(context),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 2,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(24.0),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _Circle(context),
+                      _Circle(context),
+                      _Circle(context),
+                      _Circle(context),
+                      _Circle(context),
+                    ],
+                  ),
+                ],
               ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CustomTextFormField(
-                    hintText: 'id',
-                    onChanged: (value) {},
-                  ),
+            Container(
+              height: MediaQuery.of(context).size.height / 2,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(24.0),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CustomTextFormField(
-                    hintText: 'password',
-                    obscureText: true,
-                    onChanged: (value) {},
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('로그인'),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 16.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: CustomTextFormField(
+                      hintText: 'id',
+                      onChanged: (value) {},
                     ),
-                    SizedBox(width: 16.0),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('회원가입'),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: CustomTextFormField(
+                      hintText: 'password',
+                      obscureText: true,
+                      onChanged: (value) {},
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('로그인'),
+                      ),
+                      SizedBox(width: 16.0),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('회원가입'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
